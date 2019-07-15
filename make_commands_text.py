9ece5ff27a482c1262f54ae0917b8c571f90bc70
Samuel Stentz <ss3410@amarel1.amarel.rutgers.edu>
2019-07-15 12:38:32 -0400

    sub

--- make_commands_text.py
+++ make_commands_text.py
@@ -5,6 +5,8 @@ import itertools
 parser = argparse.ArgumentParser()
 parser.add_argument("-txt","--text_file", help = "name of text file")
 parser.add_argument("-p","--protease", help = "name of the protease")
+parser.add_argument("-st", "substrate_txt", help= "name of text file containing substrates to include")
+
 args = parser.parse_args()
 
 if args.protease is None:
@@ -17,12 +19,17 @@ if args.text_file is None:
 else:
 	txt = args.text_file
 
+if args.substrate_txt is None:
+	st = ""
+else:
+	st = "-st {}".format(args.substrate_txt)
+
 od = "/projects/f_sdk94_1/EnzymeModelling/SilentFiles/{}"
 s = "/projects/f_sdk94_1/EnzymeModelling/CrystalStructure/{}.pdb"
 substrate_HCV = "A{}__C.ASHL"
 p1 = 203
 
-template = "python design_protease.py -s {} -od {} -sf {} -site 198 -ps \"198-202\" -cons ly104.cst -cr 72 96 154 -dprot 0 -dpep 0 -p1 {}" 
+template = "python design_protease.py -s {} -od {} -sf {} -site 198 -ps \"198-202\" -cons ly104.cst -cr 72 96 154 -dprot 0 -dpep 0 -p1 {} {}" 
 
 aa = "ACDEFGHIKLMNPQRSTVWY"
 
@@ -32,7 +39,7 @@ for (i,j,k) in itertools.product(*[aa for i in range(3)]):
 	var = i + j + k
 	sf = substrate_HCV.format(var)
 	command = template.format(s.format(protease),
-		od.format(protease), sf, p1)
+		od.format(protease), sf, p1, st)
 	fh.write("{}\n".format(command))
 fh.close()
 
